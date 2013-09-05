9c0040ca814b0fcfa6d18a990b34e8f944bfdd79
Maleesa Pollock <maleesapollock@gmail.com>
2013-09-05 16:40:17 -0700

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ InventoryApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'items#index'
+  root 'sessions#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
